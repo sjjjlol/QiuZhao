c98e4b2a857b01d6652f9ae64417d01be618b6d1
sjjjlol <shenjianle02@gmail.com>
2025-04-11 22:06:13 +0800

    4/11

--- 手撕代码/code/src/singleton.java
+++ 手撕代码/code/src/singleton.java
@@ -14,4 +14,10 @@ public class singleton {
         }
         return instance;
     }
+
+    public static void main(String[] args) {
+        singleton instance1 = singleton.getInstance();
+        singleton instance2 = singleton.getInstance();
+        System.out.println(instance1.equals(instance2));
+    }
 }
